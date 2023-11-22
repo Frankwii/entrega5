8ba89c02f26dce9ba686597dfcf8714f3228c6cb
Frankwii <frank.william.hammond@gmail.com>
2023-11-22 18:55:10 +0100

    ACP

--- main.R
+++ main.R
@@ -1,9 +1,13 @@
+library(tidyverse)
+
 df=read.table("europa.dat")
+Country=rownames(df)
+rownames(df)=NULL
+Country %>% length
+df<-df %>% mutate(Country) 
+colnames(df)=c("Agriculture", "Mines", "Factories","Energy","Construction","Service_industries","Finance","Social_services","Trans_comm","Country")
 
-str(df)
-
-
-
+df <- df %>% relocate("Country",1)
 
 
 
